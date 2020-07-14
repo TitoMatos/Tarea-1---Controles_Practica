476cfac20d286fc63232421c26893c5e038df629
TitoMatos <55864156+TitoMatos@users.noreply.github.com>
2020-07-14 00:18:36 -0400

    Agregar archivos de proyecto.

---
+++ Controles_Practica_1/Controles_Practica_1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Controles_Practica_1
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private async void BtbMostrar_Clicked(object sender, EventArgs e)
+        {
+
+            int año = Int32.Parse(txtFecha.Text);
+            int edad = 2020 - año;
+            string Situación;
+
+
+            if (check1.IsChecked == true)
+            {
+                Situación = " estoy casado/a "; 
+            }else{
+                Situación = " no estoy casado/a ";
+
+            }
+            var profesión = picker1.SelectedItem;
+
+            await DisplayAlert("Mensaje", "Hola mi nombre es " + txtNombre.Text + " " + txtApellido.Text + ", tengo " + edad + " años de edad y nací en " + txtLugar.Text + " , en la actualidad" + Situación + "y soy profesional en el area de " + profesión + ".", "OK");
+
+        }
+
+    }
+}
